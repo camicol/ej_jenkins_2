cb4884f0c1273a6d9099809080878d4d94b1c2d2
camicol <69647737+camicol@users.noreply.github.com>
2021-07-23 10:26:39 +0200

    4th version of class

--- LowerToUpper.java
+++ LowerToUpper.java
@@ -2,7 +2,7 @@ public class LowerToUpper
 {
   public static void main(String[] input)
     {
-        public void lowerToUpper(String strLower) 
+        public static void lowerToUpper(String strLower) 
         {
           strUpper = strLower.toUpperCase();
           System.out.print("Equivalent Word/Name in Uppercase : " +strUpper);
