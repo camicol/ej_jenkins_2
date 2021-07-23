18f778ebb7a0c9e8d8ac31db7f1ded3b85cb2695
camicol <69647737+camicol@users.noreply.github.com>
2021-07-23 10:18:38 +0200

    Second version of class

--- lowerToUpper.java
+++ lowerToUpper.java
@@ -1,14 +1,11 @@
-import java.Util.Scanner;
 public class LowerToUpper
 {
   public static void main(String[] input)
     {
-        String strUpper, strLower;
-        Scanner scan = new Scanner(System.in);
-    
-        strUpper = scan.nextLine();
-        
-        strLower = strUpper.toUpperCase();
-        System.out.print("Equivalent Word/Name in Uppercase : " +strLower);
+        public void lowerToUpper(String strLower) 
+        {
+          strUpper = strLower.toUpperCase();
+          System.out.print("Equivalent Word/Name in Uppercase : " +strUpper);
+        }
     }
 }
